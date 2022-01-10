09624428fb6832d14a397eebed3baabde33d1b48
MashedPotato <78328022+MashedTuna@users.noreply.github.com>
2022-01-10 19:36:19 +0700

    Update rrtypes.js

--- commands/reactionrole/rrtypes.js
+++ commands/reactionrole/rrtypes.js
@@ -35,7 +35,7 @@ module.exports = class extends Command {
   const embedType = new MessageEmbed()
   .setAuthor(message.author.tag, message.author.displayAvatarURL())
   .setDescription(`\`Type 1\` - React adds the role, unreacting removes the role\n\`Type 2\` - Reacting will give the role but unreaction won't remove the role\n\`Type 3\` - Reacting will remove the user's role and unreacting won't give it back\n\`Type 4\` - When reacting it will remove the role, unreacting will add the role\n\`Type 5\` - Same concept as number 3 but removes the user's reaction\n\`Type 6\` - React to recieve the role and react again to remove the role`)
-  .setFooter(`https://github.com/peterhanania/reaction-roles`)
+  .setFooter(`https://github.com/MashedTuna/Reaction-Role`)
    .setColor(client.color.red)
 
 message.channel.send(embedType)
