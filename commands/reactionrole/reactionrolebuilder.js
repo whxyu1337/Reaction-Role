d9aad5239cd3b4c7c455839b02b1e13dab5d5a28
MashedPotato <78328022+MashedTuna@users.noreply.github.com>
2022-01-10 19:35:30 +0700

    Update reactionrolebuilder.js

--- commands/reactionrole/reactionrolebuilder.js
+++ commands/reactionrole/reactionrolebuilder.js
@@ -32,24 +32,24 @@ module.exports = class extends Command {
   const missingPermEmbed = new MessageEmbed()
   .setAuthor(`Missing User Permissions`, message.author.displayAvatarURL())
   .setDescription(`${fail} The following command the **Administrator** Permission`)
-  .setFooter(`https://github.com/peterhanania/reaction-roles`)
+  .setFooter(`https://github.com/MashedTuna/Reaction-Role`)
 
 
  
   const cancelledEmbed = new MessageEmbed()
     .setAuthor(message.author.tag, message.author.displayAvatarURL())
   .setDescription(`${success} Reaction Builder Cancelled!`)
-  .setFooter(`https://github.com/peterhanania/reaction-roles`)
+  .setFooter(`https://github.com/MashedTuna/Reaction-Role`)
 
   const cancelledEmbed2 = new MessageEmbed()
     .setAuthor(message.author.tag, message.author.displayAvatarURL())
   .setDescription(`${fail} Wrong Response, Reaction Builder Cancelled!`)
-  .setFooter(`https://github.com/peterhanania/reaction-roles`)
+  .setFooter(`https://github.com/MashedTuna/Reaction-Role`)
 
     const timeEnd = new MessageEmbed()
     .setAuthor(message.author.tag, message.author.displayAvatarURL())
   .setDescription(`${fail} Time has Ended, Reaction Builder Cancelled!`)
-  .setFooter(`https://github.com/peterhanania/reaction-roles`)
+  .setFooter(`https://github.com/MashedTuna/Reaction-Role`)
 const filter = m => m.author.id === message.author.id
 
 message.channel.send("Please specify a channel! **[channel / ID]**\n\n**Type Cancel to cancel**").then(() => {
@@ -98,7 +98,7 @@ message.channel.send("Please specify a channel! **[channel / ID]**\n\n**Type Can
     if (!emoji) return message.channel.send(new MessageEmbed()
      .setAuthor(message.author.tag, message.author.displayAvatarURL())
   .setDescription(`${fail} Provide me with a valid Emoji`)
-  .setFooter(`https://github.com/peterhanania/reaction-roles`)
+  .setFooter(`https://github.com/MashedTuna/Reaction-Role`)
    .setColor(client.color.red)
     );
 
@@ -106,7 +106,7 @@ message.channel.send("Please specify a channel! **[channel / ID]**\n\n**Type Can
     if (isCustomEmoji(emoji)) return message.channel.send(new MessageEmbed()
      .setAuthor(message.author.tag, message.author.displayAvatarURL())
   .setDescription(`${fail} Do Not use custom Emojis!`)
-  .setFooter(`https://github.com/peterhanania/reaction-roles`)
+  .setFooter(`https://github.com/MashedTuna/Reaction-Role`)
    .setColor(client.color.red)
     );
 
@@ -119,7 +119,7 @@ await messageID.react(emoji)
      .setAuthor(message.author.tag, message.author.displayAvatarURL())
   .setDescription(`${fail} Please Provide a valid Emoji.`)
   .setColor(client.color.red)
-  .setFooter(`https://github.com/peterhanania/reaction-roles`));
+  .setFooter(`https://github.com/MashedTuna/Reaction-Role`));
 }
               
               message.channel.send("__**Finally Pick:**__\n\n`1` - React adds the role, unreacting removes the role\n`2`- Reacting will give the role but unreaction won't remove the role\n`3` - Reacting will remove the user's role and unreacting won't give it back\n`4` - When reacting it will remove the role, unreacting will add the role\n`5` - Same concept as number 3 but removes the user's reaction\n`6` - React to recieve the role and react again to remove the role").then(() => {
@@ -137,7 +137,7 @@ await messageID.react(emoji)
                 .addField('Message ID', ID, true)
                 .addField('Message', `[Jump To Message](${messageID.url})`, true)
                 .addField('Role', role, true)
-                .setFooter('https://github.com/peterhanania/reaction-roles')
+                .setFooter('https://github.com/MashedTuna/Reaction-Role')
                 ).then(async () => {
                   messageID.react(emoji)
                   
